d42221ed5a138cf1c248eea2a570f4845829e004
sodeq <osloguys@gmail.com>
2019-01-02 18:06:08 +0700

    a little play with date on hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -1,5 +1,16 @@
+#! python script
+
+import datetime as dt
 import numpy as np
 import pandas_datareader as pdr
 
-msg = "Hello sodeq"
-print(msg)
+endDate = dt.datetime.now()
+nDaysAgo = 10
+startDate = (endDate - dt.timedelta(days = nDaysAgo))
+
+print(endDate.date())
+print(startDate.date())
+
+df = pdr.data.DataReader('CPIN.JK', 'yahoo', startDate, endDate)
+
+print(df.tail(5))
